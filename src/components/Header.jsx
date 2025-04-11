623faee4d92289585f0f1982b1cfd71243f6fcc8
ddjirikian <ddjirikian@andreani.com>
2025-04-11 19:35:25 -0300

    useMemo

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,8 @@
 export default function Header({cart}) {
 
     //State derivado
-    const isEmpty = () => cart.length === 0
-    const cartTotal = () => cart.reduce( (total, item) => total + (item.price * item.quantity ), 0)
+    const isEmpty = useMemo( () => cart.length === 0, [cart])
+    const cartTotal = useMemo( () => cart.reduce( (total, item) => total + (item.price * item.quantity ), 0), [cart]) 
 
     return (
         <header className="py-5 header">
@@ -20,7 +20,7 @@ export default function Header({cart}) {
                           <img className="img-fluid" src="/img/carrito.png" alt="imagen carrito" />
 
                           <div id="carrito" className="bg-white p-3">
-                              { isEmpty() ? (
+                              { isEmpty ? (
                                 <p className="text-center">El carrito esta vacio</p>
                               ) : (
                                 <>
@@ -72,7 +72,7 @@ export default function Header({cart}) {
                                         </tbody>
                                     </table>
                                 
-                                    <p className="text-end">Total pagar: <span className="fw-bold">${cartTotal()}</span></p>
+                                    <p className="text-end">Total pagar: <span className="fw-bold">${cartTotal}</span></p>
                                     <button className="btn btn-dark w-100 mt-3 p-2">Vaciar Carrito</button>
                                 </>
                               )}
